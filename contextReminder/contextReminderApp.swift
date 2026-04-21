83f1f1adabe57598ed34e65f0d25e1678f9c7bc9
aidberto <xbertovic@gmail.com>
2026-04-21 14:54:49 +1000

    Initial Commit

---
+++ contextReminder/contextReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  contextReminderApp.swift
+//  contextReminder
+//
+//  Created by Aiden Bertovic on 21/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct contextReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
